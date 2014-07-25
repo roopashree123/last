1b525b49ff91d14cfa88d96cde75e2b042e55684
onevega <roopashree@onevega.com>
2014-07-25 13:40:06 +0530

    first

---
+++ app/models/product.rb
@@ -0,0 +1,40 @@
+class Product < ActiveRecord::Base
+  attr_accessible :brief_description, 
+				  :category_name, 
+				  :description, 
+				  :discounts, 
+				  :list_price, 
+				  :offer_date,
+				  :product_name, 
+				  :product_price,
+				  :quantity, 
+				  :image
+
+validates :brief_description,  presence: true
+validates :category_name,  presence: true
+validates :description,  presence: true
+validates :discounts,  presence: true
+validates :list_price,  presence: true
+validates :offer_date,  presence: true
+validates :product_name,  presence: true
+validates :product_price,  presence: true
+validates :quantity,  presence: true
+validates :image,  presence: true   
+
+mount_uploader :image, ImageUploader
+
+
+def self.search(search)
+  if search
+    find(:all, :conditions => ['product_name LIKE  ?', "%#{search}%"])
+	find(:all, :conditions => ['product_name LIKE  ?', "%#{search}%"])
+	
+	
+	 
+  else
+    find(:all)
+  end
+end
+
+
+end
